08bbc786525bee1a8a76832071d69e02b0fd762c
Terje Landa <terje.landa@intele.com>
2012-02-17 13:03:28 +0100

    Third...

--- com/intele/Test.java
+++ com/intele/Test.java
@@ -3,7 +3,12 @@ package com.intele;
 public class Test {
 
 
+	public Test() {
+		System.out.println("Hello from constructor!");
+	}
+
 	public void main() {
+		Test test = new Test();
 		System.out.println("Hello GIT!");
 	}
 }
